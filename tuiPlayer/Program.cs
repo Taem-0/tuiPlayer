cecf2b118fcbdfe725dbddaf58ee4274d613e0b5
Mult0 <taezarex@gmail.com>
2026-07-07 23:54:12 +0800

    Add project files.

---
+++ tuiPlayer/Program.cs
@@ -0,0 +1,30 @@
+﻿using Terminal.Gui.App;
+using Terminal.Gui.Configuration;
+using Terminal.Gui.ViewBase;
+using Terminal.Gui.Views;
+using tuiPlayer;
+using tuiPlayer.Views;
+
+ConfigurationManager.Enable(ConfigLocations.All);
+
+Application
+    .Create()
+    .Run<MainWindow>()
+    .Dispose();
+
+namespace tuiPlayer
+{
+    public sealed class MainWindow : Runnable
+    {
+        public MainWindow()
+        {
+            queuePane queuePane = new()
+            {
+                Width = Dim.Fill(),
+                Height = Dim.Fill()
+            };
+
+            Add(queuePane);
+        }
+    }
+}
